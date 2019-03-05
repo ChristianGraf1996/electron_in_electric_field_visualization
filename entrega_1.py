3783167cff38f9ffd286b1fa0f6b79f2a2ab74fa
scherzono1 <cgrafa96@gmail.com>
2019-03-05 01:12:35 +0100

    finished v1 :D

--- entrega_1.py
+++ entrega_1.py
@@ -13,4 +13,7 @@ print(electron.maxx,electron.maxy,electron.time)
 plt.plot(electron.x_array, electron.y_array, 'ro')
 plt.axis([0,0.03,0,0.015])
 plt.plot(electron.maxx,electron.maxy,'bo')
+formatList = ["{:.4f}".format(electron.maxx),"{:.4f}".format(electron.maxy),"{:.4f}".format(electron.time*math.pow(10,6))]
+text = "max X: {}, max Y: {}, time: {}*10^-6".format(*formatList)
+plt.text(electron.maxx,electron.maxy,text,fontsize = 15)
 plt.show()
